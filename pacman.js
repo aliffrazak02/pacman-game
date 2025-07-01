ab66bebfd843d32e5953fc0d4fc0e4d06cd5dabe
Aliff Iman <aliffmlg@gmail.com>
2025-07-02 05:59:49 +0800

    Add image assets and load images in pacman.js

--- pacman.js
+++ pacman.js
@@ -6,9 +6,51 @@ const boardWidth = colCount * tileSize;
 const boardHeight = rowCount * tileSize;
 let context;
 
+//images
+let blueGhostImage;
+let orangeGhostImage;
+let pinkGhostImage;
+let redGhostImage;
+let pacmanUpImage;
+let pacmanDownImage;
+let pacmanLeftImage;
+let pacmanRightImage;
+let wallImage;
+
 window.onload = function() {
     board = document.getElementById("board");
     board.width = boardWidth;
     board.height = boardHeight;
     context = board.getContext("2d");
+    loadImages();
+}
+
+function loadImages() {
+    blueGhostImage = new Image();
+    blueGhostImage.src = "images/blueGhost.png";
+
+    orangeGhostImage = new Image();
+    orangeGhostImage.src = "images/orangeGhost.png";
+
+    pinkGhostImage = new Image();
+    pinkGhostImage.src = "images/pinkGhost.png";
+
+    redGhostImage = new Image();
+    redGhostImage.src = "images/redGhost.png";
+
+    pacmanUpImage = new Image();
+    pacmanUpImage.src = "images/pacmanUp.png";
+
+    pacmanDownImage = new Image();
+    pacmanDownImage.src = "images/pacmanDown.png";
+
+    pacmanLeftImage = new Image();
+    pacmanLeftImage.src = "images/pacmanLeft.png";
+
+    pacmanRightImage = new Image();  
+    pacmanRightImage.src = "images/pacmanRight.png";
+    
+    wallImage = new Image();
+    wallImage.src = "images/wall.png";   
+
 }
