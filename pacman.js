19b17fad580b7fa42791e23794c523ac34ed6cc9
Aliff Iman <aliffmlg@gmail.com>
2025-07-02 05:51:59 +0800

    Initialize game board setup in pacman.js

---
+++ pacman.js
@@ -0,0 +1,14 @@
+let board;
+const rowCount = 21;
+const colCount = 19;
+const tileSize = 32;
+const boardWidth = colCount * tileSize;
+const boardHeight = rowCount * tileSize;
+let context;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.width = boardWidth;
+    board.height = boardHeight;
+    context = board.getContext("2d");
+}
